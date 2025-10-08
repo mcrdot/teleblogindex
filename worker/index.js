024fb3003dcba1ac6c0993ed7f43255845ee1cf5
mcrdot <macrotiserpk@gmail.com>
2025-10-08 07:42:46 +0500

    2nd update

--- worker/index.js
+++ worker/index.js
@@ -1,3 +1,21 @@
+/**
+ * Welcome to Cloudflare Workers! This is your first worker.
+ *
+ * - Run "npm run dev" in your terminal to start a development server
+ * - Open a browser tab at http://localhost:8787/ to see your worker in action
+ * - Run "npm run deploy" to publish your worker
+ *
+ * Learn more at https://developers.cloudflare.com/workers/
+ */
+
+// export default {
+//   async fetch(request, env, ctx) {
+//     // You can view your logs in the Observability dashboard
+//     console.info({ message: 'Hello World Worker received a request!' }); 
+//     return new Response('Hello World!');
+//   }
+// };
+
 // TeleBlog Authentication Worker
 export default {
   async fetch(request, env, ctx) {
@@ -29,7 +47,7 @@ export default {
       });
     }
 
-    return new Response('Not Found', { status: 404 });
+    return new Response('UniversalAppEngine is Active', { status: 404 });
   }
 }
 
